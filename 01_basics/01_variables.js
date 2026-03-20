37480166623a55a2789621bfecf370d01218f03d
Lalit Sharma <yourgithubemail@gmail.com>
2026-03-20 22:59:13 +0530

    first

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+ const accountId=144553
+ let accountEmail="hitesh@gmail.com"
+ accountPassword="212221212"
+ accountCity="jaipur"
+// const does not change
+// accountId=2  not allowed
+// variable -->  let
+
+accountEmail="hc@hc.com"
+console.log(accountId);
+/*
+prefer not to use var 
+bqz of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail , accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should he high
+    console.log("hitesh")
+
+    let name="lalit"
+    let age=18
+    let isLoggedIn=false
+
+    //num ber=> 2 to power 53
+    //bigint
+    //string=> " "
+    //boolean=> true/false
+    //null=> standalone value
+    //undefined=>
+        //symbol=> unique
+
+    //object
+
+    console.log(typeof "lalit")
+    
+    console.lof(typeof undefined); //undefined
+    console.log(typeof null); //object
