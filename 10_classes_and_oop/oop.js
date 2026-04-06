9cb53d5820f906bf33acb02298cb96d97d47fd0b
Lalit Sharma <yourgithubemail@gmail.com>
2026-04-06 19:38:29 +0530

    oop

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -25,3 +25,25 @@ const user = {
 // Constructor function => new keyword
 //allow to make multiple instances from single object
 
+function User(username, loginCount , isLoggedIn){
+    this.username= username;   // variable = value
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn ;
+
+    this.greeting = function(){
+        console.log(`welcome ${this.username}`);
+        
+    }
+
+    return this 
+    // function is implicitly defined
+}
+
+const userOne =new  User("lalit",12,true)
+const userTwo =new User("sharma" ,11,false)
+console.log(userOne.constructor);
+// console.log(userTwo);
+
+// jb hmm new keyword use krte hai to ek empty object generate hota hai
+// ek constructor function call hota hai new keyword ke karan
+
